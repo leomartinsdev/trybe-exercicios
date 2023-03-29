4c84440f337c220d1e87e5129536c2101f793750
Leonardo Martins <leonardogustavo@outlook.com>
2023-03-28 21:22:35 -0300

    fim do exercício

---
+++ front-end/secao-2-introducao-ao-react/dia-1-hello-world-no-react/exercicio-fixacao/src/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class About extends React.Component {
+  render() {
+    return (
+      <>
+      <h1>Leonardo Martins</h1>
+      <p>25 anos de idade, brasileiro nascido em São Paulo.
+        Apaixonado por tecnologia, jogos e Fórmula 1.
+      </p>
+      <h2>Minhas Habilidades</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Jest</li>
+      </ul>
+      </>
+    )
+  }
+}
