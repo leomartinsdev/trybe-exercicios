0b0230c8294ab1d87bcc28eb107d6a7e52cc5b0a
Leonardo Martins <leonardogustavo@outlook.com>
2023-07-21 11:39:52 -0300

    Exercícios de back-end 3-1

---
+++ back-end/secao-3-nodejs/dia-1-nodejs-runtime-assincrono/my-node-project/readFile.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+
+async function main() {
+  try {
+    const data = await fs.readFile('./meu-arquivo.txt', 'utf-8');
+    console.log(data);
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo: ${err.message}`);
+  }
+}
+
+main();
+
+// Diferente do readFileSync, o readFile é assíncrono e mais performático.
