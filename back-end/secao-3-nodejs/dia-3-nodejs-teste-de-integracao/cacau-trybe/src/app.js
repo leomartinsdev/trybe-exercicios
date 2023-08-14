54462c2f45df60bf87cd92046b34b2d5514f1144
Leonardo Martins <leonardogustavo@outlook.com>
2023-08-14 14:54:07 -0300

    O último teste não funcionou

--- back-end/secao-3-nodejs/dia-3-nodejs-teste-de-integracao/cacau-trybe/src/app.js
+++ back-end/secao-3-nodejs/dia-3-nodejs-teste-de-integracao/cacau-trybe/src/app.js
@@ -8,6 +8,18 @@ app.get('/chocolates', async (req, res) => {
   res.status(200).json({ chocolates });
 });
 
+app.get('/chocolates/total', async (req, res) => {
+  const chocolates = await cacauTrybe.getAllChocolates();
+  return res.status(200).json({ totalChocolates: chocolates.length })
+});
+
+app.get('/chocolates/search', async (req, res) => {
+  const { name } = req.query;
+  const chocolates = await cacauTrybe.findChocolateByName(name);
+  res.status(chocolates.length === 0 ? 404 : 200)
+    .json(chocolates);
+});
+
 app.get('/chocolates/:id', async (req, res) => {
   const { id } = req.params;
   // Usamos o Number para converter o id em um inteiro
@@ -16,10 +28,20 @@ app.get('/chocolates/:id', async (req, res) => {
   res.status(200).json({ chocolate });
 });
 
+app.put('/chocolates/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, brandId } = req.body;
+  const updatedChocolate = await cacauTrybe.updateChocolate(Number(id), { name, brandId });
+
+  if (updatedChocolate) return res.status(200).json({ chocolate: updatedChocolate });
+  res.status(404).json({ message: 'chocolate not found' });
+});
+
 app.get('/chocolates/brand/:brandId', async (req, res) => {
   const { brandId } = req.params;
   const chocolates = await cacauTrybe.getChocolatesByBrand(Number(brandId));
   res.status(200).json({ chocolates });
 });
+
 
 module.exports = app;
--- back-end/secao-3-nodejs/dia-3-nodejs-teste-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
+++ back-end/secao-3-nodejs/dia-3-nodejs-teste-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
@@ -121,4 +121,74 @@ describe('Testando a API Cacau Trybe', function() {
       ]);
     });
   });
+
+  describe('Usando o método GET em /chocolates/total', function() {
+    it('Retorna a quantidade total de Chocolates (4)', async function () {
+      const response = await chai
+        .request(app)
+        .get('/chocolates/total');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal({ totalChocolates: 4 });
+    });
+  });
+
+  describe('Usando o método GET em /chocolates/search', function() {
+    it('Retorna chocolates que contem Mo no nome', async function () {
+      const response = await chai.request(app).get('/chocolates/search?name=Mo');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal([
+        {
+          id: 3,
+          name: 'Mon Chéri',
+          brandId: 2,
+        },
+        {
+          id: 4,
+          name: 'Mounds',
+          brandId: 3,
+        },
+      ]);
+    });
+
+    it('Retorna array vazio caso não encontro nenhum chocolate', async function () {
+      const response = await chai.request(app).get('/chocolates/search?name=XYZ');
+
+      expect(response.status).to.be.equal(404);
+      expect(response.body).to.deep.equal([]);
+    });
+  });
+
+  describe('Usando o método PUT em /chocolates/:id', function () {
+    it('Atualiza um chocolate existente', async function () {
+      const response = await chai
+        .request(app)
+        .put('/chocolates/1').send({
+        name: 'Mint Pretty Good',
+        brandId: 2,
+      });
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolate).to.deep.equal({
+        id: 1,
+        name: 'Mint Pretty Good',
+        brandId: 2,
+      });
+    });
+      
+    it('Se o chocolate não existe, gera um erro', async function () {
+      const response = await chai
+        .request(app)
+        .put('/chocolates/555').send({
+        name: 'Mint Pretty Good',
+        brandId: 2,
+        });
+      
+      expect(response.status).to.be.equal(404);
+      expect(response.body).to.deep.equal({
+        message: 'chocolate not found',
+      });
+    });
+  });
 });
